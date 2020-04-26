5aaaeac4d1f11daf1a9f6c47e79e9639a5a561fa
Artandrey <artandrey7777@gmail.com>
2020-04-26 11:50:01 +0300

    updaaaaaaaaaate

--- javascript/module-10/webpack-starter-kit-master/src/index.js
+++ javascript/module-10/webpack-starter-kit-master/src/index.js
@@ -133,9 +133,51 @@ const createTemplate = function(){
         markup = template(el)
         ul.innerHTML += markup;
     }
-    for (let i = 0; i < jsonData.length; i++) {
-        listGenerator(i)
-    }
 }
 
 createTemplate();
+
+
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+let body = document.querySelector('body');
+
+let input = document.querySelector('.switch__input');
+
+
+const setLight = function () {
+  body.classList.add(Theme.LIGHT);
+  body.classList.remove(Theme.DARK);
+};
+
+const setDark = function () {
+  body.classList.remove(Theme.LIGHT);
+  body.classList.add(Theme.DARK);
+};
+const theme = localStorage.getItem('theme');
+console.log(theme);
+if (theme === 'light') input.checked = false
+else if (theme === 'dark') input.checked = true
+else input.checked = false
+
+if (input.checked) setDark();
+else setLight();
+
+
+
+input.addEventListener('change', () => {
+  if (input.checked) {
+      setDark();
+      localStorage.setItem('theme', 'dark');
+  }
+  else {
+      setLight();
+      localStorage.setItem('theme', 'light');
+  }
+  console.log(input.checked);
+});
+
+
